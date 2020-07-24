d1191b3a5c4238b3ba6618302f3ca2791ff5d758
om17 <64308013+om17@users.noreply.github.com>
2020-07-24 10:36:45 +0530

    om17.github.io

--- main.js
+++ main.js
@@ -1,40 +1,74 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAtF7yZFZ18xzl_mz3PFu4mQrhXrXNL4Gs",
-    authDomain: "contact-form-df695.firebaseapp.com",
-    databaseURL: "https://contact-form-df695.firebaseio.com",
-    projectId: "contact-form-df695",
-    storageBucket: "contact-form-df695.appspot.com",
-    messagingSenderId: "765685836411",
-    appId: "1:765685836411:web:bcec69b2794b8e09b990de",
-    measurementId: "G-Y8ZWF3HZRQ"
+    apiKey: "AIzaSyDHLRZWpZaRoG0IZeqa-9S2ZREjgOlQKdg",
+    authDomain: "contactform-41be0.firebaseapp.com",
+    databaseURL: "https://contactform-41be0.firebaseio.com",
+    projectId: "contactform-41be0",
+    storageBucket: "contactform-41be0.appspot.com",
+    messagingSenderId: "798127909563",
+    appId: "1:798127909563:web:9661ffbc2526bb38fd979e",
+    measurementId: "G-P21C4L8KV0"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  var messagesRef = firebase.database().ref('messages');
-document.getElementById('contactForm').addEventListener('submit',submitForm);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database().ref('message');
+
+
+document.getElementById('contactform'),addEventListener('submit',submitForm);
 
 function submitForm(e){
-	e.preventDefault();
-	var first_name = getInputVal('first_name');
-	var last_name = getInputVal('last_name');
-	var email = getInputVal('email');
-	var telephone = getInputVal('telephone');
-	var comments = getInputVal('comments');
-	saveMessage(first_name,last_name,email,telephone,comments);
 
+    e.preventDefault();
+
+
+     var Name = getinput('uname');
+     var Phone = getinput('phone');
+     var Gender = getinputg('gender');
+     var Email = getinput('email');
+     var Message = getinput('message');
+
+    savemsg(Name, Phone, Gender, Email, Message);
+
+    document.querySelector('.confirm').style.display = 'block';
+
+    setTimeout(function(){
+
+        document.querySelector('.confirm').style.display = 'none';
+    },3500);
+
+    document.getElementById('contactform').reset();
 }
-function getInputVal(id) {
-	return.document.getElementById(id).value;
-	// body...
+
+function getinput(id){
+    return document.getElementById(id).value;
 }
-function saveMessage(first_name,last_name,email,telephone,comments){
-	var newMessageRef = messagesRef.push();
-	newMessageRef.set({
-		first_name:first_name,
-		last_name:last_name,
-		email:email,
-		telephone:telephone,
-		comments:comments
-	});
+
+function getinputg(id){
+    var g;
+    if(gender[0].checked)
+    {
+        g = gender[0].value;
+        return g;
+    }
+    else if(gender[1].checked)
+    {
+        g = gender[1].value;
+        return g;
+    }
+    else if(gender[2].checked)
+    {
+        g = gender[2].value;
+        return g;
+    }
+}
+
+function savemsg(Name ,Phone, Gender ,Email, Message){
+    var newdb = db.push();
+    newdb.set({
+
+        NAME: Name,
+        PHONE: Phone,
+        GENDER: Gender,
+        EMAIL: Email,
+        MESSAGE: Message   
+    });
 }
